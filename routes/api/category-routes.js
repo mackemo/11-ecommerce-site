9953cb93c2c173811a6d0f6137429d5d0bb6ebda
mackemo <mackenzielmoore14@gmail.com>
2024-06-11 13:41:38 -0400

    completed delete request, destroying atcategory id, in try/catch block

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,8 +63,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+
+    if (!categoryData) {
+      res.status(404).json({message: 'No category with that id'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json({message: 'Internal Server Error'});
+  }
 });
 
 module.exports = router;
