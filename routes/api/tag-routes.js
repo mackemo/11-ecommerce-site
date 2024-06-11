82b1dd186ee1ded2334d318d31ac7d60fb5a3943
mackemo <mackenzielmoore14@gmail.com>
2024-06-11 15:38:57 -0400

    fixed a route in tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Category }],
+      include: [{ model: Product }],
     })
 
     if (!tagData) {
