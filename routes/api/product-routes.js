33c36e2a06639304ff8f7a3ee5da5ae311dafe64
mackemo <mackenzielmoore14@gmail.com>
2024-06-11 14:43:02 -0400

    completed delete route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,8 +113,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+
+    if (!productData) {
+      res.status(404).json({message: 'No product with that id'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json({message: 'Internal Server Error'});
+  }
 });
 
 module.exports = router;
