555f8853e70f73e0228deccb12fa3bdbf9156dc8
mackemo <mackenzielmoore14@gmail.com>
2024-06-11 13:39:11 -0400

    completed put request for updating a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,6 +23,7 @@ router.get('/:id', async (req, res) => {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     })
+
     if (!categoryData) {
       res.status(404).json({message: 'No category with that id'});
       return;
@@ -43,8 +44,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    })
+    if (!categoryData) {
+      res.status(404).json({message: 'No category with that id'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json({message: 'Internal Server Error'});
+  }
 });
 
 router.delete('/:id', (req, res) => {
